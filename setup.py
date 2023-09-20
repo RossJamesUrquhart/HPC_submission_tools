e209599e37f2c47afa9485f719040290995158bc
Ross Urquhart <rossurquhart1998@gmail.com>
2023-09-20 12:05:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@ setup(
   download_url = 'https://github.com/RossJamesUrquhart/HPC_submission_tools/archive/refs/tags/v0.2.2-alpha.tar.gz',    # I explain this later on
   keywords = ['HPC', 'Command line'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'os',
-          'pathlib',
-          'shutil'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
